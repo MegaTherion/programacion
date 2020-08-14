d5ab7d3b2823e360cee2b8afa6d12c118db31899
Christian Gomez <christian.gomez.4@gmail.com>
2020-08-14 18:34:07 -0400

    primer commit

---
+++ resources/views/bienvenido.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenido a la materia Programacion avanzada</title>
+    <style>
+    h1 {
+        color: blue;
+    }
+    .rojo {
+        color: red;
+    }
+    p.verde {
+        color: green;
+    }
+    #dep {
+        color: purple;
+    }
+    
+    </style>    
+</head>
+<body>
+    <h1>Programacion avanzada</h1>
+    <p class="rojo">Este es el portal de la materia</p>
+    <p><b>Gestión: </b> 2 / 2020</p>
+    <ul>
+        <li>Pera</li>
+        <li>Uva</li>
+    </ul>
+
+    <h1 class="rojo">Herramientas</h1>
+    <p class="verde">Tenemos las siguientes herramientas.</p>
+    <ol>
+        <li>Martillo</li>
+        <li>Pala</li>
+    </ol>
+
+    <h1 id="dep">Deportes</h1>
+
+    <ul>
+        <li>Basket</li>
+        <li>Natación</li>
+        <li>Ajedrez</li>
+    </ul>
+
+    <button>Ingresar al sistema</button>
+
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('hola', function () {
+    return '<h1>Hola USFA!!!</h1>';
+});
+
+Route::get('christian', function () {
+    return '<h2>Hola a todos!</h2>';
+});
+
+Route::get('user/{codigo}', function ($codigo) {
+    return 'Usuario '.$codigo;
+});
+
+Route::get('saludar/{nombre}', function ($nombre) {
+    // return '<h1>Hola ' . $nombre . '</h1>'
+    return "<h1>Hola $nombre, cómo estás?</h1>";
+});
+
+Route::get('acercade', function () {
+    return view('bienvenido');
+});
+
+Route::get('bootstrap', function() {
+    return view('bootstrap');
+});
